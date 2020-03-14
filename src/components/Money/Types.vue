d4bd9d29410e7105ceaa60fee9c532e11aa27bb6
carmine <hl13886803841@sina.com>
2020-03-14 14:56:39 +0800

    完成Types组件的功能

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -9,10 +9,17 @@
 
 <script lang="ts">
    import Vue from 'vue';
-   import {Component} from 'vue-property-decorator';
+   import {Component, Prop} from 'vue-property-decorator';
 
    @Component //Component是一个装饰器
    export default class Types extends Vue {
+
+      @Prop(Number) xxx: number | undefined;
+      //Prop告诉 Vue xxx是一个props属性，而不是普通的data
+      // Number告诉Vue  在运行时 xxx的类型是Number（也就是你在给xxx传值的时候 得传number类型的值）
+      //xxx是props的属性名
+      //number|undefined 是告诉Vue TS在编译成JS时，xxx的类型（没有给xxx初始值的话 就要给一个undefined类型,所以以后在使用xxx的时候都得先判断是不是undefined）
+
       type = '-'; //-代表流出 +代表流入  默认为流出
       selectType(type: string) {
          if (type !== '-' && type !== '+') {
@@ -22,26 +29,6 @@
       }
    }
 </script>
-
-
-<!--<script>-->
-<!--   export default {-->
-<!--      name: 'Types',-->
-<!--      data() {-->
-<!--         return {-->
-<!--            type: '-' //-代表流出 +代表流入  默认为流出-->
-<!--         }-->
-<!--      },-->
-<!--      methods: {-->
-<!--         selectType(type) {-->
-<!--            if (type !== '-' && type !== '+') {-->
-<!--               throw new Error('type类型未知')-->
-<!--            }-->
-<!--            this.type = type-->
-<!--         }-->
-<!--      }-->
-<!--   }-->
-<!--</script>-->
 
 <style scoped lang="scss">
    @import "~@/assets/style/helper.scss";
